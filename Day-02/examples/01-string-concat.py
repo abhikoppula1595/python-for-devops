436ab927c7921d56e24b5c88be2311073fd9b31c
abhikoppula1595 <abhyudhaykoppula15@gmail.com>
2025-09-16 10:51:13 -0500

    chnages in concat

--- Day-02/examples/01-string-concat.py
+++ Day-02/examples/01-string-concat.py
@@ -2,3 +2,16 @@ str1 = "Hello"
 str2 = "World"
 result = str1 + " " + str2
 print(result)
+
+
+first = "abhi"
+last = "koppula"
+result = first + " " + last
+print(result)
+
+
+
+past = "newyork"
+present = "texas"
+result = past + " " + present
+print("place:", result)
