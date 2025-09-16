43d90c851a7564adc9fb5edc0eb48833fbc3a234
abhikoppula1595 <abhyudhaykoppula15@gmail.com>
2025-09-16 09:47:46 -0500

    added some example in len

--- Day-02/examples/01-string-len.py
+++ Day-02/examples/01-string-len.py
@@ -1,3 +1,7 @@
 text = "Python is awesome"
 length = len(text)
 print("Length of the string:", length)
+
+text = "abhi is becoming successful"
+length = len(text)
+print("Length of the string:", length)
