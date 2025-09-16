ec4c83915558a28a4557dc8c6c25e1fc0d3bf16c
abhikoppula1595 <abhyudhaykoppula15@gmail.com>
2025-09-16 16:17:23 -0500

    chnages 1

--- Day-02/examples/02-float.py
+++ Day-02/examples/02-float.py
@@ -18,3 +18,26 @@ print("Division:", result4)
 # Rounding
 result5 = round(3.14159265359, 2)  # Rounds to 2 decimal places
 print("Rounded:", result5)
+
+
+# Float variables
+num3 = 3.5
+num4 = 1.22
+
+result1 = num3 + num4
+print("addition:", result1)
+
+result2 = num3 - num4
+print("subtraction:", result2)
+
+result3 = num3 * num4
+print("multiplication:", result3)
+
+result4 = num3 / num4
+print("division:", result4)
+
+result5 = round(1.123456, 3)
+print("rounded:", result5)
+
+
+
