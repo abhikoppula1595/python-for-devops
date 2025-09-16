79f6418ac8aac6b5b0764dc6812a65286122f06e
abhikoppula1595 <abhyudhaykoppula15@gmail.com>
2025-09-16 09:59:07 -0500

    updated missed lines

--- Day-02/examples/01-string-len.py
+++ Day-02/examples/01-string-len.py
@@ -5,3 +5,7 @@ print("Length of the string:", length)
 text = "abhi is becoming successful"
 length = len(text)
 print("Length of the string:", length)
+
+text = "DevOps Engineer"
+length = len(text)
+print("length of string is:" length)
