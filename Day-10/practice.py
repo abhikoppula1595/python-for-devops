2cb9716cc18f4802a7e227eae6c9fcdd971e8737
abhikoppula1595 <abhyudhaykoppula15@gmail.com>
2025-10-13 14:43:23 -0500

    day10 changes

--- Day-10/practice.py
+++ Day-10/practice.py
@@ -3,10 +3,17 @@ import os
 folders = input("please provide list of folder names with spaces in between: ").split()
 
 for folder in folders:
-    files = os.listdir(folder)
-    print("-------------listing files for the folders: " + folder)
+
+    try:
+        files = os.listdir(folder)
+    except FileNotFoundError:
+        print("please provide a valid folder name")
+        continue
+
+    print("\n-------------listing files for the folders: " + folder, flush=true)
 
     for file in files:
         print(file)
+    
     
 
